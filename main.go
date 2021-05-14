b0072aad6f171092050ff43179bae199156567d8
gimmeabeer <53146540+krastomer@users.noreply.github.com>
2021-05-15 01:36:34 +0700

    update v5

--- main.go
+++ main.go
@@ -3,18 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	port := os.Getenv("PORT")
+	port := 1323
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
-	address := fmt.Sprintf("%s:%s", "0.0.0.0", port)
+	address := fmt.Sprintf("%s:%v", "0.0.0.0", port)
 	e.Start(address)
 }
